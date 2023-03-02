62db986b9972c8651ae777abc43ff155a3f55310
Zain Ghanim <96354337+ZainGhanim@users.noreply.github.com>
2023-03-01 19:50:15 -0500

    Update javaMethods.java

--- javaMethods.java
+++ javaMethods.java
@@ -239,24 +239,7 @@ public class Main{
         }
         return counter;
     }
-    public static Node deleteNode(Node lst, int target){
-        if(lst == null || countNodes(lst) == 0){
-            return lst;
-        }
-        Node iter = lst;
-        if(lst.val == target){
-            Node som = lst.next;
-            lst.next = null;
-            return som;
-        }
-        while(iter.next != null){
-            if(iter.next.val == target){
-                iter.next = iter.next.next;
-            }
-            iter = iter.next;
-        }
-        return lst;
-    }
+    
     public static int getDecimalValue(Node head) {
         StringBuilder sb = new StringBuilder();
         Node track = head;
@@ -285,6 +268,325 @@ public class Main{
         }
         return newString;
         }
+    
+    //--------------------------------------- Linked Lists -------------------------------------------------
+    public static node build(int[] input) {
+        node head = new node(-1, (node)null);
+        node curr = head;
+        int[] var3 = input;
+        int var4 = input.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            int i = var3[var5];
+            curr.next = new node(i, (node)null);
+            curr = curr.next;
+        }
+
+        return head.next;
+    }
+    
+    public static node delete(node lst, int target) {
+        if (lst == null) return null;
+        if (lst.val == target) {
+            node retVal = lst.next;
+            lst.next = null;
+            return retVal;
+        }
+        node iter = lst;
+        while (iter.next != null) {
+            // do some work
+            if (iter.next.val == target) {
+                iter.next = iter.next.next;
+            }
+            iter = iter.next;
+        }
+        return lst;
+    }
+    
+    public boolean hasCycle(node lst) {
+        if (lst == null) return false;
+        node slow = lst;
+        node fast = lst.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+    
+    public node merge(node l1, node l2) {
+        node dummy = new node(-1, null);
+        node iter = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                iter.next = l1;
+                l1 = l1.next;
+            } else {
+                iter.next = l2;
+                l2 = l2.next;
+            }
+            iter = iter.next;
+        }
+        if (l1 != null) iter.next = l1;
+        if (l2 != null) iter.next = l2;
+        return dummy.next;
+    }
+    
+    public int add(node lst1, node lst2){
+        int sum1 = 0;
+        int factor = 1;
+        while(lst1 != null){
+            sum1 += lst1.val = factor;
+            factor *= 10;
+            lst1 = lst1.next;
+        }
+        int sum2 = 0;
+        factor = 1;
+        while(lst2 != null){
+            sum2 += lst2.val * factor;
+            factor *= 10;
+            lst2 = lst2.next;
+        }
+        return sum1 + sum2;
+    }
+    
+    public static node partition(node head, int x) {
+        node lt = new node(-1,null);
+        node ltHead = lt;
+        node gt = new node(-1,null);
+        node gtHead = gt;
+        while(head != null){
+            if(head.val < x){
+                ltHead.next = head;
+                ltHead = ltHead.next;
+            } else {
+                gtHead.next = head;
+                gtHead = gtHead.next;
+            }
+        }
+        ltHead.next = gt.next;
+        return ltHead.next;
+    }
+    
+    public static Node deleteNode(Node lst, int target){
+        if(lst == null || countNodes(lst) == 0){
+            return lst;
+        }
+        Node iter = lst;
+        if(lst.val == target){
+            Node som = lst.next;
+            lst.next = null;
+            return som;
+        }
+        while(iter.next != null){
+            if(iter.next.val == target){
+                iter.next = iter.next.next;
+            }
+            iter = iter.next;
+        }
+        return lst;
+    }
+    
+    public node kthLast(node input, int k) {
+        int counter = 0;
+        node head = input;
+        while(head != null){
+            counter++;
+            head = head.next;
+        }
+        if (k > counter || k <= 0){
+            return input;
+        } else if (input == null) {
+            return null;
+        }
+        node save = null;
+        node tracker = input;
+        while(tracker != null){
+            if(counter == k){
+                save = tracker;
+            }
+            counter--;
+            tracker = tracker.next;
+        }
+        return save;
+    }
+    
+    public node thirdList(node lst){
+        node first = lst;
+        node third = lst;
+        for(int i=0; i<3 && third != null; i++){
+            third = third.next;
+        }
+        while(third != null){
+            first = first.next;
+            third = third.next;
+        }
+        return first;
+    }
+
+    //lst is sorted
+    public node removeDups(node lst){
+        if(lst == null) return null;
+        node iter = lst;
+        while(iter != null){
+            while(iter.next != null && iter.next.val == iter.val){
+                iter.next = iter.next.next;
+            }
+            iter = iter.next;
+        }
+        return lst;
+    }
+    
+    public static int count(node head) {
+        int count = 1;
+        for(node current = head; current.next != null; ++count) {
+            current = current.next;
+        }
+        return count;
+    }
+    
+    //ideas that backup the OddEvenList question
+    public static Node merge(Node one, Node two){
+        Node dummy = new Node(-1,null);
+        Node pointer1 = dummy;
+        int i = 1;
+        while(one != null && two != null){
+            if(i % 2 == 0){
+                pointer1.next = one;
+                one = one.next;
+            } else {
+                pointer1.next = two;
+                two = two.next;
+            }
+            pointer1 = pointer1.next;
+            i++;
+        }
+        if(one != null){
+            pointer1.next = one;
+        }
+        if(two != null){
+            pointer1.next = two;
+        }
+        return dummy.next;
+        //code in phone
+    }
+    
+    public static Node split(Node input){
+        Node one = new Node(-1,null);
+        Node pointer1 = one;
+        Node two = new Node(-1,null);
+        Node pointer2 = two;
+        int i = 1;
+        while(input != null){
+            if(i % 2 == 0){
+                pointer1.next = input;
+                input = input.next;
+                pointer1 = pointer1.next;
+                if(input == null){
+                    pointer2.next = null;
+                }
+            }else{
+                pointer2.next = input;
+                input = input.next;
+                pointer2 = pointer2.next;
+                if(input == null){
+                    pointer1.next = null;
+                }
+            }
+            i++;
+        }
+        System.out.println(one.next);
+        return two.next;
+    }
+    
+    // LeetCode Medium OddEvenList Question
+    public ListNode oddEvenList(ListNode head) {
+        ListNode one = new ListNode(-1, null);
+        ListNode pointer1 = one;
+        ListNode two = new ListNode(-1,null);
+        ListNode pointer2 = two;
+        int i = 1;
+
+        while(head != null){
+            if(i % 2 == 0){
+                pointer1.next = head;
+                head = head.next;
+                pointer1 = pointer1.next;
+                if (head == null){
+                    pointer2.next = null;
+                }
+            } else{
+                pointer2.next = head;
+                head = head.next;
+                pointer2 = pointer2.next;
+                if (head == null){
+                    pointer1.next = null;
+                }
+            }
+            i++;
+        }
+        pointer2.next = one.next;
+        return two.next;
+    }
+    
+    // Leetcode Medium swapPairs Question
+    public ListNode swapPairs(ListNode head) {
+        ListNode one = new ListNode(-1, null);
+        ListNode pointer1 = one;
+        ListNode two = new ListNode(-1,null);
+        ListNode pointer2 = two;
+        int i = 1;
+
+        while(head != null){
+            if(i % 2 == 0){
+                pointer1.next = head;
+                head = head.next;
+                pointer1 = pointer1.next;
+                if(head == null){
+                    pointer2.next = null;
+                }
+            } else {
+                pointer2.next = head;
+                head = head.next;
+                pointer2 = pointer2.next;
+                if(head == null){
+                    pointer1.next = null;
+                }
+            }
+            i++;
+        }
+
+        ListNode three = new ListNode(-1,null);
+        ListNode pointer3 = three;
+        i = 1;
+
+        while(one.next != null && two.next != null){
+            if(i % 2 == 0){
+                pointer3.next = two.next;
+                two.next = two.next.next;
+                pointer3 = pointer3.next;
+            } else {
+                pointer3.next = one.next;
+                one.next = one.next.next;
+                pointer3 = pointer3.next;
+            }
+            i++;
+        }
+
+        if(one.next != null){
+            pointer3.next = one.next;
+        }
+        
+        if(two.next != null){
+            pointer3.next = two.next;
+        }
+
+        return three.next;
+    }
+    
 
     public static void main(String[] args) {
         String word = "codeleet";
